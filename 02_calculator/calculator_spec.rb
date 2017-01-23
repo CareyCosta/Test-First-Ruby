ae6ef12cb64eb20c4364d12c12921527b8b5cddd
Carey Costa <careyxcosta@gmail.com>
2017-01-23 15:10:33 -0500

    calculator started

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -23,13 +23,17 @@
 #
 # # Bonus
 #
-# There are also some bonus exercises for when you finish the regular ones. The bonus round will help teach you test-driven *development*, not simply test-guided *learning*.
+# There are also some bonus exercises for when you finish the
+#regular ones. The bonus round will help teach you test-driven
+#*development*, not simply test-guided *learning*.
 #
-# Your mission, should you choose to accept it, is to write *tests* for three new methods:
+# Your mission, should you choose to accept it, is to write
+#*tests* for three new methods:
 #
 # * `multiply` which multiplies two numbers together
 # * `power` which raises one number to the power of another number
-# * `[factorial](http://en.wikipedia.org/wiki/Factorial)` (check Wikipedia if you forgot your high school math).
+# * `[factorial](http://en.wikipedia.org/wiki/Factorial)`
+#(check Wikipedia if you forgot your high school math).
 #
 #
 

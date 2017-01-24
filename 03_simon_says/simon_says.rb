8792a32c092e6bcc7caddd573666ed9ac8ac7bfe
Carey Costa <careyxcosta@gmail.com>
2017-01-24 14:14:58 -0500

    simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,3 +1,4 @@
+
 def echo(string)
   "#{string}"
 end
@@ -6,7 +7,16 @@ def shout(string)
   "#{string}".upcase
 end
 
-def repeat(string)
-  new_string = "#{string} #{string}"
-  return new_string
+def repeat(string, n = 2)
+  new_string = "#{string} " * n
+  return new_string.strip
+end
+
+def start_of_word(string, n)
+  new_string = "#{string}".split(//)
+  return new_string.first
+
+  elsif "#{n}" == 2
+    then new_string.first(2)
+  end
 end
